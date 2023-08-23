e47a61c6f9e354c0ab7fb9b777cc75c45c2843ef
ZabDi10 <keko10@hotmail.es>
2023-08-23 16:07:13 +0200

    18/08

---
+++ paginaAdidas/js/script.js
@@ -0,0 +1,22 @@
+
+
+ /* window.addEventListener("load",()=>{
+
+
+
+    let imagenes=document.querySelectorAll(".zapa");
+    imagenes.forEach(items=>{
+        items.addEventListener("mouseover",()=>{
+            items.style.width="15%";
+
+        });
+        items.addEventListener("mouseout", ()=>{
+            items.style.width="25%";
+
+
+
+        })
+    })
+})
+
+ */
