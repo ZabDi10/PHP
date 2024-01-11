1fb5f996f743678d88721b1c7595de4c0e484042
ZabDi10 <keko10@hotmail.es>
2024-01-11 14:30:13 +0100

    11/01

--- cepav3/vista/header.php
+++ cepav3/vista/header.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/styleIndex.css">
+    <link rel="stylesheet" href="css/styleAdmin.css">
+    <link rel="stylesheet" href="css/styleAlta.css">
+    <link rel="stylesheet" href="css/styleDash.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
